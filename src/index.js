bb74b965e2e61ac25f28c658d8630db40bdcd9c1
sanazniamati <sanaz.niamati1991@gmail.com>
2022-06-22 10:19:42 +0430

    first

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Stage, Layer, Line } from "react-konva";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+  return (
+    <Stage width={window.innerWidth} height={window.innerHeight}>
+      <Layer>
+        <Line
+          y={50}
+          points={[10, 70, 40, 23, 150, 60, 250, 20]}
+          stroke={"blue"}
+          strokeWidth={10}
+          lineCap={"round"}
+          lineJoin={"round"}
+          /*
+           * line segments with a length of 29px with a gap
+           * of 20px followed by a line segment of 0.001px (a dot)
+           * followed by a gap of 20px
+           */
+          dash={[29, 20, 0.001, 20]}
+          draggable
+        />
+      </Layer>
+    </Stage>
+  );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
